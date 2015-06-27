33dfea6453ecdd5e6c42bfd4498f3d05a2546b38
Chris Proctor <chris.proctor@gmail.com>
2015-06-27 07:32:10 -0700

    added needed import statement

--- code/cards/calculate_war.py
+++ code/cards/calculate_war.py
@@ -1,7 +1,9 @@
+from war import War
 
 # play out one thousand games of war.
 results = [War().play() for _ in range(1000)]
 
+# and then report on how they went.
 print("After one thousand games of war, the shortest game had {} battles, the longest had {} battles, and the average was {}".format(
     min(results),
     max(results),
